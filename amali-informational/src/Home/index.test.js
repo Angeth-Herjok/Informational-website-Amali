f35c836a83f9598a3bcd5c28cacee24964fa233c
Ann-Okoyo <annmuyale@gmail.com>
2023-09-07 16:00:26 +0300

    fixed conflict

--- amali-informational/src/Home/index.test.js
+++ amali-informational/src/Home/index.test.js
@@ -1,63 +1,10 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
 import HomePage from "./index";
 
-let container = null;
-
-beforeEach(() => {
-  
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-describe("HomePage", () => {
-  it("changes background image after a certain interval", () => {
-    jest.useFakeTimers();
-
-    
-    act(() => {
-      render(<HomePage />, container);
-    });
-
-    
-    act(() => {
-      jest.advanceTimersByTime(3000);
-    });
-
-    
-    const backgroundElement = container.querySelector(".background");
-    const backgroundImage = window.getComputedStyle(backgroundElement).getPropertyValue("background-image");
-    expect(backgroundImage).toContain("kiprop.png");
-
-    
-    act(() => {
-      jest.advanceTimersByTime(3000);
-    });
-
-    
-    const newBackgroundImage = window.getComputedStyle(backgroundElement).getPropertyValue("background-image");
-    expect(newBackgroundImage).toContain("eliudkipchoge.jpg");
-
-    
-    act(() => {
-      jest.advanceTimersByTime(3000);
-    });
-
-    
-    const finalBackgroundImage = window.getComputedStyle(backgroundElement).getPropertyValue("background-image");
-    expect(finalBackgroundImage).toContain("chepngetich.png");
-
-    
-    act(() => {
-      jest.clearAllTimers();
-    });
+describe("HomePage Component", () => {
+  it("renders without errors", () => {
+    const { container } = render(<HomePage />);
+    expect(container).toBeInTheDocument();
   });
 });
