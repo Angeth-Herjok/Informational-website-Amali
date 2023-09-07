b18c4bf844d532ec2250a3c3ab22f5987d33910a
Maurine-Jebet <maurinejebett@gmail.com>
2023-09-07 15:00:12 +0300

    updated the about feature

--- amali-informational/src/App.js
+++ amali-informational/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
-<<<<<<< HEAD
 import AboutUs from './Components/About-Us';
 import TrendingCampaigns from './Components/TrendingCampaigns';
-=======
 import AboutUs from './About-Us';
->>>>>>> 7c273cf66839ab9382e5592c28b36ef3a5c1422b
 
 const App =()=> {
   return (
     <div>
       <AboutUs/>
-<<<<<<< HEAD
 
       <TrendingCampaigns/>
-=======
->>>>>>> 7c273cf66839ab9382e5592c28b36ef3a5c1422b
     </div>
   );
 }
