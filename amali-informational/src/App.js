fc175ceb4547b914c6c328bf7907fa0b75726d3c
Rabecca-Chepkemboi <124131757+Rabecca-Chepkemboi@users.noreply.github.com>
2023-09-07 13:45:44 +0300

    Update App.js

--- amali-informational/src/App.js
+++ amali-informational/src/App.js
@@ -1,16 +1,9 @@
-feature/TrendingCampaigns
 import './App.css';
 import TrendingCampaigns from './Components/trendingCampaigns';
-import React from 'react';
-import './App.css';
-import OurTeam from './Component/OurTeam';
-
-
 
 function App() {
   return (
     <div>
-feature/TrendingCampaigns
       <TrendingCampaigns/>
       <OurTeam/>
     </div>
